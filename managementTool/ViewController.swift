af7e077a8fe8c3071ecebca9d62ff8a424977c46
takeda yuri <>
2020-08-07 00:34:02 +0900

    設定

---
+++ managementTool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  managementTool
+//
+//  Created by takeda yuri on 2020/08/07.
+//  Copyright © 2020 takeda yuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
